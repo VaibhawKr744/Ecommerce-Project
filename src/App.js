47d4830198c9dc4a28526ed6d0c9677bf3817920
VaibhawKr744 <honeykr744@gmail.com>
2020-11-10 18:53:19 +0530

    firebase sign in/up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Homepage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -21,10 +21,26 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(onSnapshot => {
+          this.setState({
+            currentUser: {
+              id: onSnapshot.id,
+              ...onSnapshot.data()
+            }
+          },
+          () => {
+            console.log(this.state);
+          }
+          );
+        });
+      }
+      else {
+        this.setState({ currentUser: userAuth});
+      }
     });
 
   }
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -3,9 +3,13 @@ import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'><SignIn /></div>
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+        <SignUp />
+        </div>
 );
 
 export default SignInAndSignUpPage;
