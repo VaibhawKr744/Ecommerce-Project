51e5b7397b226dca0102367e9d60a4eeab7bfc5b
VaibhawKr744 <honeykr744@gmail.com>
2020-11-22 15:35:36 +0530

    group cart item

--- src/components/custom-button/custom-button.js
+++ src/components/custom-button/custom-button.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const customButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+    <button className={ `${inverted ? 'inverted' : ''} 
+    ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
         {children}
     </button>
 );
 
-export default customButton;
+export default CustomButton;
